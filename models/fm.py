1befc3272591d9cc9e05022863faad1994b8f823
imyutaro <yutarofortech@gmail.com>
2019-10-08 10:12:05 +0900

    fm is working on kaggle kernel, dataloader is not implemented

--- models/fm.py
+++ models/fm.py
@@ -2,10 +2,15 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-np.random.seed(0)
-torch.manual_seed(0)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+# for reproducibility
+def seed_everything(seed=1234):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+seed_everything()
 
 class FM(nn.Module):
     """
@@ -21,20 +26,23 @@ class FM(nn.Module):
     """
     def __init__(self, n, k):
         super(FM, self).__init__()
-        self.w_0 = torch.randn(1, dtype=torch.float32, requires_grad=True)
-        self.w = nn.Parameter(torch.rand(n, 1))
-        self.V = nn.Parameter(torch.rand(n, k))
+        self.w_0 = nn.Parameter(torch.ones(1))
+        self.w = nn.Parameter(torch.randn(n, 1))
+        self.V = nn.Parameter(torch.randn(n, k))
 
     def forward(self, x):
         """
         x is input and has to be one-hot vector
         """
-        linear = torch.mm(self.w, x)
+        # 2nd term
+        linear = torch.mm(x, self.w)
+
+        # 3rd term
         inter1 = torch.mm(x, self.V).pow(2)
         inter2 = torch.mm(x.pow(2), self.V.pow(2))
-        intersection = 0.5*(inter1-inter2).sum()
+        intersection = 0.5*(inter1-inter2).sum(1, keepdim=True)
         
-        y = w_0 + linear + intersection
+        y = self.w_0 + linear + intersection
         return y
 
 if __name__=="__main__":
